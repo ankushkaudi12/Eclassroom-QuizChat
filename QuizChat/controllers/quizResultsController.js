e8ccbda3bf4ef9e58fb63098694fad3c7f23e182
ankushkaudi12 <ankushkaudi12@gmail.com>
2025-04-24 14:38:18 +0530

    Calculate score and fetch score completed

---
+++ QuizChat/controllers/quizResultsController.js
@@ -0,0 +1,44 @@
+const db = require('../database/connection');
+
+const calculateAndStoreScores = async (req, res) => {
+  try {
+    const query = `
+      INSERT INTO quiz_results (student_id, quiz_id, score)
+      SELECT
+        sa.student_id,
+        q.quiz_id,
+        COUNT(*) AS score
+      FROM student_answers sa
+      JOIN questions q ON sa.question_id = q.id
+      WHERE sa.selected_answer = q.correct_answer
+      GROUP BY sa.student_id, q.quiz_id
+      ON DUPLICATE KEY UPDATE score = VALUES(score);
+    `;
+
+    await db.execute(query);
+
+    res.status(200).json({ message: 'Scores calculated and stored successfully.' });
+  } catch (error) {
+    console.error('Error calculating scores:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const getQuizResults = async (req, res) => {
+    const { quiz_id } = req.params.quiz_id;
+    
+    try {
+        const query = `
+        SELECT
+            SELECT * FROM quiz_results WHERE quiz_id = ?;
+        `;
+    
+        const [results] = await db.execute(query, [quiz_id]);
+        res.status(200).json(results);
+    } catch (error) {
+        console.error('Error fetching quiz results:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+module.exports = { calculateAndStoreScores, getQuizResults };
--- QuizChat/routes/router.js
+++ QuizChat/routes/router.js
@@ -3,6 +3,7 @@ const { addAnnouncements, upload, getAnnouncements, deleteAnnouncement, updateAn
 const { addQuiz, getQuizzes, deleteQuiz, editQuiz } = require("../controllers/quizController");
 const { addQuestions, getQuestions } = require("../controllers/questionsController");
 const { addStudentAnswers } = require("../controllers/studentAnswersController");
+const { calculateAndStoreScores, getQuizResults } = require("../controllers/quizResultsController");
 
 const router = express.Router();
 
@@ -11,11 +12,14 @@ router.post("/announcements/add", upload.single("file"), addAnnouncements);
 router.post("/quiz/add", addQuiz);
 router.post("/quiz/add/questions", addQuestions);
 router.post("/quiz/submission", addStudentAnswers)
+router.post("/quiz/calculatescore", calculateAndStoreScores)
 
 /* GET methods */
 router.get("/announcements/:classroom_id", getAnnouncements);
 router.get("/quiz/:course_id", getQuizzes);
 router.get("/quiz/questions/:quiz_id", getQuestions);
+router.get("/quiz/results/:quiz_id", getQuizResults);
+
 
 /* DELETE methods */
 router.delete("/announcements/delete/:id", deleteAnnouncement);
